02f3a760e13c7e778064d02a6dc6df82c21a54cc
ongzh <ongzh23@gmail.com>
2022-06-16 11:14:31 -0400

    responsive fixes

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -16,26 +16,28 @@ function Schedule(){
     return <div id="schedule" style={{padding: "5%", backgroundColor:"rgb(15,15,15)"}}>
         <h1 style={{fontFamily: "Georgia, serif", fontSize: "calc(15px + 3vw)", textAlign: "center", color: "red"}}>Schedule</h1>
         <br/>
+        <p style={{fontSize: "23px", color: "red"}}><strong> *Schedule and location for Fall 2022 will be updated in late August. </strong>
+        </p>
         <p style={{fontSize: "20px"}}>We usually train twice a week, on Wednesdays and Sundays. As of right now, classes are located outdoors at Jeanne-Mance Park and will take place weather permitting (we are currently waiting on the green light for indoor classes).
         </p>
-        <Card className="m-5 border-0 shadow" style={styles.card}>
+        <Card className="m-2 border-0 shadow" style={styles.card}>
             <Row>
             <Col>
               <Card.Body>
               <Card.Text as="h4" style={styles.cardText}>
               <h2 style={{fontFamily: "Georgia, serif"}}>Membership fees</h2>
-        <ul>
+        <ul style={{fontSize: "20px"}}>
             <li>Membership fees are $45 per semester or $80 for two semesters.</li>
             <li style={{color: "red"}}> Due to uncertainty of the Fall 2021 semester, we would be collecting fees of $10 per month instead </li>
         </ul>
         <h2 style={{fontFamily: "Georgia, serif"}}>Fall 2021</h2>
-        <ul>
+        <ul style={{fontSize: "20px"}}>
             <li>Wednesday: 4.00P.M.- 6.00P.M.</li>
             <li>Sunday: 3.00P.M. - 5.00P.M.</li>
             <li>Location: Jeanne-Mance Park (Right behind the McGill Gym)</li>
         </ul>
         <h2 style={{fontFamily: "Georgia, serif"}}>Winter 2022</h2>
-        <ul style={{color:"red"}}>
+        <ul style={{color:"red", fontSize: "20px"}}>
             <li>
             Due to the current Covid-19 situation, all boxing club activities are on hold for Winter 2022. Do join our facebook group to get the latest updates.
             </li>
@@ -43,7 +45,7 @@ function Schedule(){
               </Card.Text>
               </Card.Body>
             </Col>
-                <Col>
+                <Col className="d-none d-lg-block">
                     <Card.Img src={schedule} style={styles.cardImage}/>
                 </Col>
             </Row>
