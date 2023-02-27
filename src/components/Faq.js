e18c8c95ee2e5cf0ef35378b36b6b9b793354a4c
Ong <ong2323@gmail.com>
2023-02-27 14:20:39 -0500

    css and font changes

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,36 +1,38 @@
+import "../styles/Faq.css"
+
 function Faq(){
     return <div id="faq" style={{padding: "5%", color: "black",backgroundColor:"rgba(230, 230, 230, 0.5)"}}>
         <h1 style={{fontSize: "calc(15px + 3vw)", textAlign: "center", color: "crimson"}}>Frequently Asked Questions</h1>
         <br/>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>How do i join the club?</li>
         </ul>
         <p >Simply show up to one of our training sessions and approach one of our friendly execs!</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>Are beginners allowed?</li>
         </ul>
         <p>Beginners are most definitely welcome! Most people who have joined have little to no experience with boxing.</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>Do you offer free trials?</li>
         </ul>
         <p>The first two classes are free. After which you can choose to pay either by the semester or by the year.</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>Do I need to bring my own equipment?</li>
         </ul>
         <p>The club provides equipment to get you started. However, it is strongly encouraged for members to purchase their own handwraps and gloves. You will need your own mouthguard to spar (optional).</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>How do I pay the membership fees?</li>
         </ul>
         <p>We accept both cash and e-transfer to mcgillboxingclub@ssmu.ca</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>Do I show have to show up to both training sessions per week?</li>
         </ul>
         <p>You can show up whenever you like. However, staying commited and training regularly is the fastest way to improve!</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>I can only show up to one session per week, can I get a discount in my membership fees?</li>
         </ul>
         <p>Unfortunately, to remain fair to all our members, we cannot give a discount in membership</p>
-        <ul style = {{fontSize: "25px"}}>
+        <ul>
             <li>If i submitted the waiver last academic year, do I have to resubmit it again?</li>
         </ul>
         <p>Yes, we will require you to resubmit it every academic year.</p>
--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,20 +1,22 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import ScrollspyNav from "react-scrollspy-nav";
+import logo from "../data/mbc-front-logo.png"
+
 
 function Navigationbar() {
     return <div>
         <ScrollspyNav offset={-20} scrollTargetIds={["about", "schedule", "team", "membership","faq"]} activeNavClass="active" scrollDuration="100">
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top" id="navbar">
                 <Container>
-                    <Navbar.Brand href="#home"><h2 style={{color:"crimson"}}>McGill Student Boxing Club</h2></Navbar.Brand>
+                    <Navbar.Brand color="crimson" href="#home" >McGill Student Boxing Club</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#about"><h3 class='section-header'>About</h3></Nav.Link>
-                            <Nav.Link href="#schedule"><h3 class='section-header'>Schedule</h3></Nav.Link>
-                            <Nav.Link href="#team"><h3 class='section-header'>Team</h3></Nav.Link>
-                            <Nav.Link href="#membership"><h3 class='section-header'>Membership</h3></Nav.Link>
-                            <Nav.Link href="#faq"><h3 class='section-header'>FAQ</h3></Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#schedule">Schedule</Nav.Link>
+                            <Nav.Link href="#team">Team</Nav.Link>
+                            <Nav.Link href="#membership">Membership</Nav.Link>
+                            <Nav.Link href="#faq">FAQ</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
