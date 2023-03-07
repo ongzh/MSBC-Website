597156cd2a16a15bbad71d9be197a92c48b5aa99
Ong <ong2323@gmail.com>
2023-03-06 21:06:48 -0500

    schedule redesign

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,55 +1,36 @@
 import schedule from "../data/schedule.jpg";
-import { Card, Row, Col } from 'react-bootstrap';
+import "../styles/Schedule.css";
 
-const styles = {
-    card: {
-      backgroundColor: 'rgba(230,230,230, 0.5)',
-      padding: '3rem'
-    },
-    cardImage: {
-      objectFit: 'cover',
-      height: '100%'
-    },
-    cardText:{
-        color: 'black',
-    }
-}
 
 function Schedule(){
-    return <div id="schedule" style={{padding: "5%", backgroundColor: "rgba(250, 250, 250, 0.7)"}}>
-        <h1 className="section-header">Schedule</h1>
-        <br/>
-        <h2 style={{fontSize: "23px", color: "crimson"}}><strong> Current Schedule (Updated Winter 2023). </strong>
-        </h2>
-        
-        <Card className="m-2 border-0 shadow" style={styles.card}>
-            <Row>
-            <Col>
-              <Card.Body>
-              <Card.Text style={styles.cardText}>
-              
+    return <div id="schedule" style={{padding: "7%", backgroundColor: "rgba(250, 250, 250, 0.7)"}}>
+        <h1 className="section-header">Training Schedule</h1>
+        <p>We try our best to update this schedule regularly. Follow us on social media for the latest updates! Our training schedule is usually only finalized 1-2 weeks into the start of the semester.</p>
+        <div id="schedule-box">
+            
+            <div id="schdule-img-container">
+            <img src={schedule} alt="" id="schedule-img"></img>
+            </div>
+            
+            <div id='schedule-info-container'>
+            <div id="schedule-info"> 
             <h2>Fall 2022</h2>
-            <ul style={{fontSize: "20px"}}>
-                <li>Friday: 3.00P.M.- 4.30P.M. (Starting Sept 23)</li>
-                <li>Sunday: 3.30P.M. - 5.30P.M.</li>
+            <ul className="semester-schedule">
+                <li>Fridays: 3.00P.M.- 4.30P.M. (Starting Sept 23)</li>
+                <li>Sundays: 3.30P.M. - 5.30P.M.</li>
                 <li>Location: Spin Room (Inside the McGill Gym, B2)</li>
             </ul>
             <h2> Winter 2023</h2>
-            <ul style={{fontSize: "20px"}}>
-                <li>Thursday: 4.45P.M.- 6.15P.M. (Starting Jan 19)</li>
-                <li>Sunday: 4.00P.M. - 6.00P.M.</li>
+            <ul className="semester-schedule">
+                <li>Thursdays: 4.45P.M.- 6.15P.M. (Starting Jan 19)</li>
+                <li>Sundays: 4.00P.M. - 6.00P.M.</li>
                 <li>Location: Spin Room (Inside the McGill Gym, B2)</li>
             </ul>
-              </Card.Text>
-              </Card.Body>
-            </Col>
-                <Col className="d-none d-lg-block">
-                    <Card.Img src={schedule} style={styles.cardImage}/>
-                </Col>
-            </Row>
-        </Card>
+            </div>
+            </div>
 
 
+        </div>
         
     </div>;
 
