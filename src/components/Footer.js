dbebbc58f0e5c58f14264a274528283adde61fa7
Ong <ong2323@gmail.com>
2023-03-08 13:41:53 -0500

    new footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,8 @@
 import { AiOutlineFacebook, AiOutlineMail, AiOutlineInstagram} from "react-icons/ai";
-import logo from "../data/logo.png";
+import logo from "../data/main-logo.png";
 import "../styles/Footer.css"
-/*
-<Container style={{height: "calc(110px + 15vh)"}} fluid>
-            <br/>
-            <Row><img src={logo} alt="" className="footer-logo" roundedCircle></img></Row>
-            <br/>
-            <Row>
-                <div style={{paddingLeft: "6vw"}}><a href="https://www.facebook.com/groups/146481348826920"><FaFacebookSquare color="#6bbcff" size="6vh"/></a></div>
-                <div><a href="https://www.instagram.com/mcgillssmuboxing/"><FaInstagramSquare size="6vh" color="#E1306C"/></a></div>
-                <div style={{paddingRight: "6vw"}}><a href="mailto:mcgillboxingclub@ssmu.ca"><AiOutlineMail size="6vh" color="#6bbcff" /></a></div>
-            </Row>
-        </Container>
-*/
+
+
 function Footer() {
    
     return <div className="footer"style={{backgroundColor: 'rgba(30,30,30,1)'}}> 
