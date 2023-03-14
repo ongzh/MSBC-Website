e7ce985e8371f02cbafd7167bdbfa1f09c63271f
Ong <ong2323@gmail.com>
2023-03-13 22:08:02 -0400

    fancy navbar that dissapears

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navigationbar from './components/Navigationbar';
+import NavBar from './components/NavBar';
 import About from './components/About'
 import Header from './components/Header';
 import Schedule from './components/Schedule';
@@ -10,10 +10,13 @@ import Team from './components/Team';
 import Membership from './components/Membership';
 import Faq from './components/Faq';
 
+
 function App() {
+  
+  
   return (
     <div>
-      <Navigationbar/>
+      <NavBar/>
       <Header/>
       <About/>
       <Schedule/>
