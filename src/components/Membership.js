2be8fe31dc930891a1202d484f416b15c31b63ac
Ong <ong2323@gmail.com>
2023-03-26 08:57:21 -0400

    file clean up

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,35 +1,60 @@
-import waiver from "../data/insurance.pdf"
-import "../styles/Membership.css"
-import members_pic from "../data/schedule.jpg"
-
+import waiver from "../data/files/insurance.pdf";
+import "../styles/Membership.css";
+import members_pic from "../data/schedule.jpg";
 
 //var interestForm = "https://forms.gle/AJnkozFkwJMctmRs9"
 
-function Membership(){
-    return <div id="membership" style={{padding: "5%", color: "black", npmbackgroundImage:"linear-gradient(rgb(250, 250, 250),rgb(230,230,230))"}}>
-    <h1 className="section-header">Fall 2022-Winter 2023 Membership</h1>
-    <div className="membership-container">
-    
-    <div className='membership-info-container'>
-    <h2 >Membership fees</h2>
-        <ul>
-            <li className="bag-list-logo">Membership fees are $55 per semester or $100 for two semesters.</li>
-            <li className="bag-list-logo">As a member, you are able to order training equipment through us at a discount rate! Talk to one of our execs for more info.</li>
-            <li className="bag-list-logo" style={{color: "crimson"}}> Due to the increase in costs of room booking at the McGill Gym, post-covid, there has been an increase in prices of our membership fees in 2022. </li>
-        </ul>
-    <h2>Documents</h2>
-    <ul>
-        <li className="bag-list-logo">Before you attend your first training session, please fill up the following <a href={waiver}>waiver form</a> and send it in to <a href="mailto:mcgillboxingclub@ssmu.ca">mcgillboxingclub@ssmu.ca</a>. Note that you have to submit the waiver form annually.</li>
-    </ul>
-    </div>
+function Membership() {
+  return (
+    <div
+      id="membership"
+      style={{
+        padding: "5%",
+        color: "black",
+        npmbackgroundImage:
+          "linear-gradient(rgb(250, 250, 250),rgb(230,230,230))",
+      }}
+    >
+      <h1 className="section-header">Fall 2022-Winter 2023 Membership</h1>
+      <div className="membership-container">
+        <div className="membership-info-container">
+          <h2>Membership fees</h2>
+          <ul>
+            <li className="bag-list-logo">
+              Membership fees are $55 per semester or $100 for two semesters.
+            </li>
+            <li className="bag-list-logo">
+              As a member, you are able to order training equipment through us
+              at a discount rate! Talk to one of our execs for more info.
+            </li>
+            <li className="bag-list-logo" style={{ color: "crimson" }}>
+              {" "}
+              Due to the increase in costs of room booking at the McGill Gym,
+              post-covid, there has been an increase in prices of our membership
+              fees in 2022.{" "}
+            </li>
+          </ul>
+          <h2>Documents</h2>
+          <ul>
+            <li className="bag-list-logo">
+              Before you attend your first training session, please fill up the
+              following <a href={waiver}>waiver form</a> and send it in to{" "}
+              <a href="mailto:mcgillboxingclub@ssmu.ca">
+                mcgillboxingclub@ssmu.ca
+              </a>
+              . Note that you have to submit the waiver form annually.
+            </li>
+          </ul>
+        </div>
 
-    <div className="member-pic-container">
-    <img src={members_pic} alt="" className="members-pic"></img>
-    <p>Check out our new Hayabusa training gear!</p>
+        <div className="member-pic-container">
+          <img src={members_pic} alt="" className="members-pic"></img>
+          <p>Check out our new Hayabusa training gear!</p>
+        </div>
+      </div>
+      <div className="page-divider"></div>
     </div>
-    </div>
-    <div className="page-divider"></div>
-</div>;
+  );
 }
 
 export default Membership;
--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import ScrollspyNav from 'react-scrollspy-nav';
-import logo from "../data/punch-man-logo.png"
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import ScrollspyNav from "react-scrollspy-nav";
+import logo from "../data/logos/punch-man-logo.png";
 
 function NavigationBar(props) {
   const [showNav, setShowNav] = useState(false);
   const [hovering, setHovering] = useState(false);
-  const [pastHeader, setPassHeader] = useState(false)
-  
+  const [pastHeader, setPassHeader] = useState(false);
+
   useEffect(() => {
     // Add a scroll event listener to the window to check if the user has scrolled down
-    window.addEventListener('scroll', handleScroll);
-    
+    window.addEventListener("scroll", handleScroll);
+
     // Remove the event listener when the component unmounts to prevent memory leaks
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
+
   useEffect(() => {
-    if (pastHeader){
-      setShowNav(true)
+    if (pastHeader) {
+      setShowNav(true);
     }
     // Check if the user is not hovering over the transparent div and the navbar is currently showing
     else if (!hovering && showNav) {
@@ -28,52 +28,79 @@ function NavigationBar(props) {
       const timeout = setTimeout(() => {
         setShowNav(false);
       }, 1000);
-    
+
       // Cancel the timeout if the user hovers over the navbar before it expires
       return () => clearTimeout(timeout);
     }
   }, [hovering, showNav, pastHeader]);
-  
+
   const handleScroll = () => {
     // Check if the user has scrolled down more than 100 pixels from the top of the page
-    if (window.pageYOffset >props.height){
-      console.log(props.height)
+    if (window.pageYOffset > props.height) {
+      console.log(props.height);
       setPassHeader(true);
-    }
-    else if (window.pageYOffset < props.height){
+    } else if (window.pageYOffset < props.height) {
       setPassHeader(false);
     }
 
-
-    if (window.pageYOffset > 100){
+    if (window.pageYOffset > 100) {
       setShowNav(true);
     } else {
       setShowNav(false);
     }
   };
-  
+
   const handleHoverEnter = () => {
     // Unhide the navbar when the user hovers over the transparent div
     setShowNav(true);
     setHovering(true);
   };
-  
+
   const handleHoverLeave = () => {
     // Set the hovering state to false when the user unhovers from the transparent div
     setHovering(false);
   };
-  
+
   return (
     <div>
-       {/* Transparent div that the user can hover over to unhide the navbar */}
-       <div style={{height: '50px', width: '100%', position: 'fixed', top: '0', zIndex: '999'}} onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}></div>
-      
+      {/* Transparent div that the user can hover over to unhide the navbar */}
+      <div
+        style={{
+          height: "50px",
+          width: "100%",
+          position: "fixed",
+          top: "0",
+          zIndex: "999",
+        }}
+        onMouseEnter={handleHoverEnter}
+        onMouseLeave={handleHoverLeave}
+      ></div>
+
       {/* Navbar that hides/shows based on the showNav state */}
-      <ScrollspyNav offset={-20} scrollTargetIds={["about", "schedule", "team", "membership", "faq"]} activeNavClass="active" scrollDuration="100">
-        <Navbar bg="dark" variant="dark" fixed="top" id="navbar" expand="lg" style={{transition: 'all 0.3s ease-in-out', opacity: showNav ? 1 : 0, transform: showNav ? 'translateY(0)' : 'translateY(-100%)'}}>
+      <ScrollspyNav
+        offset={-20}
+        scrollTargetIds={["about", "schedule", "team", "membership", "faq"]}
+        activeNavClass="active"
+        scrollDuration="100"
+      >
+        <Navbar
+          bg="dark"
+          variant="dark"
+          fixed="top"
+          id="navbar"
+          expand="lg"
+          style={{
+            transition: "all 0.3s ease-in-out",
+            opacity: showNav ? 1 : 0,
+            transform: showNav ? "translateY(0)" : "translateY(-100%)",
+          }}
+        >
           <Container fluid>
             <Navbar.Brand href="#home" className="nav-logo">
-              <span><img alt="" src={logo} height='30px'/>McGill Student Boxing Club</span>
+              <span>
+                <img alt="" src={logo} height="30px" />
+                McGill Student Boxing Club
+              </span>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
